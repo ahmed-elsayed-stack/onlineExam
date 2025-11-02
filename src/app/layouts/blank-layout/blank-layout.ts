e7f57305e5d46e609eec897675305a4146637f9a
Ahmed Elsayed <ahmedzekeba6@gmail.com>
2025-11-02 19:13:51 +0200

    Initial commit - setup project structure

---
+++ src/app/layouts/blank-layout/blank-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blank-layout',
+  imports: [],
+  templateUrl: './blank-layout.html',
+  styleUrl: './blank-layout.scss',
+})
+export class BlankLayout {
+
+}
